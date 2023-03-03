ebbf8689fac037463ac80cd111ee9546791907c5
VaheGasparyan <vahegsparyan@gmail.com>
2023-03-04 00:07:54 +0400

    create menu functional

---
+++ javascript/options.js
@@ -0,0 +1,35 @@
+export const heroOptions = {
+    x: 15,
+    y: 15,
+    height: 8,
+    width: 12,
+    xDelta: 1,
+    yDelta: .4
+}
+
+export const pipeFirst = {
+    x: 120,
+    y: 0,
+    height: 60,
+    width: 20,
+    xDelta: .5,
+    yDelta: 1,
+}
+
+export const pipeTwo = {
+    x: 200,
+    y: -20,
+    height: 60,
+    width: 20,
+    xDelta: .5,
+    yDelta: 1,
+}
+
+export const pipeThree = {
+    x: 280,
+    y: -10,
+    height: 60,
+    width: 20,
+    xDelta: .5,
+    yDelta: 1,
+}
--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,18 @@
 import { Hero, PipeUp, PipeBottom } from "./script_1.js";
 import { bg, heroImage, pipeUpImg, pipeBottomImg } from "./script_2.js";
 import { draw, loop } from "./script_3.js";
+import { heroOptions, pipeFirst, pipeTwo, pipeThree } from "./options.js";
 
 export const cvs = document.querySelector('canvas');
 export const menu = document.querySelector('.menu');
 export const ctx = cvs.getContext('2d');
 export const gap = 90;
+const restartButton = document.querySelector('.button');
+const options = [pipeFirst, pipeTwo, pipeThree];
 
-export const hero = new Hero(heroImage, 15, 15, 8, 12, 1, .4);
-export const pipeUp = [new PipeUp(120, 0, 60, 20, .5, 1, pipeUpImg), new PipeUp(200, -20, 60, 20, .5, 1, pipeUpImg), new PipeUp(280, -10, 60, 20, .5, 1, pipeUpImg)];
-export const pipeBottom = [new PipeBottom(120, 0, 60, 20, .5, 1, pipeBottomImg), new PipeBottom(200, -20, 60, 20, .5, 1, pipeBottomImg), new PipeBottom(280, -10, 60, 20, .5, 1, pipeBottomImg)];
+export const hero = new Hero(heroImage, heroOptions.x, heroOptions.y, heroOptions.height, heroOptions.width, heroOptions.xDelta, heroOptions.yDelta);
+export const pipeUp = [new PipeUp(pipeFirst.x, pipeFirst.y, pipeFirst.height, pipeFirst.width, pipeFirst.xDelta, pipeFirst.yDelta, pipeUpImg), new PipeUp(pipeTwo.x, pipeTwo.y, pipeTwo.height, pipeTwo.width, pipeTwo.xDelta, pipeTwo.yDelta, pipeUpImg), new PipeUp(pipeThree.x, pipeThree.y, pipeThree.height, pipeThree.width, pipeThree.xDelta, pipeThree.yDelta, pipeUpImg)];
+export const pipeBottom = [new PipeBottom(pipeFirst.x, pipeFirst.y, pipeFirst.height, pipeFirst.width, pipeFirst.xDelta, pipeFirst.yDelta, pipeBottomImg), new PipeBottom(pipeTwo.x, pipeTwo.y, pipeTwo.height, pipeTwo.width, pipeTwo.xDelta, pipeTwo.yDelta, pipeBottomImg), new PipeBottom(pipeThree.x, pipeThree.y, pipeThree.height, pipeThree.width, pipeThree.xDelta, pipeThree.yDelta, pipeBottomImg)];
 
 bg.onload = draw;
 
@@ -19,4 +22,33 @@ document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowUp') {
         hero.jumpUp(6);
     }
-})
+});
+
+restartButton.addEventListener('click', () => {
+    console.log(pipeUp)
+    options.forEach((option, index) => {
+        pipeUp[index].x = option.x;
+        pipeUp[index].y = option.y;
+        pipeUp[index].width = option.width;
+        pipeUp[index].height = option.height;
+        pipeUp[index].xDelta = option.xDelta;
+
+        pipeBottom[index].x = option.x;
+        pipeBottom[index].y = option.y;
+        pipeBottom[index].width = option.width;
+        pipeBottom[index].height = option.height;
+        pipeBottom[index].xDelta = option.xDelta;
+    });
+
+    hero.x = heroOptions.x;
+    hero.y = heroOptions.y;
+    hero.width = heroOptions.width;
+    hero.height = heroOptions.height;
+    hero.xDelta = heroOptions.xDelta;
+    hero.yDelta = heroOptions.yDelta;
+
+    pipeUp.pop();
+    pipeBottom.pop();
+
+    menu.classList.remove('active')
+});
